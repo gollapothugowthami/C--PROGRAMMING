cb0ec121887908cb9f9066adff12be40cbce7a70
gollapothugowthami <gollapothugowthami@gmail.com>
2026-01-06 15:29:07 +0530

    Add files via upload
    C  program eligible to vote

---
+++ 16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int age;
+scanf("%d",&age);
+if(age >= 18)
+{ 
+ printf(" you are eligible to vote.\n");
+}
+else
+{ 
+ printf(" you are not eligible to vote.\n");
+}
+  return 0;
+}
