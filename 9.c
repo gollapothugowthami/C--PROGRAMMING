a6a3f92d28cc6090a7494d4223c0ca2c1eb4b4a0
gollapothugowthami <gollapothugowthami@gmail.com>
2026-01-06 15:17:01 +0530

    Add files via upload
    c program upper case lower case

--- 9.c
+++ 9.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 int main(){
-char ch;
-scanf("%c",& ch);
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')    
- {
-    printf("vowel");
- }   
+  char ch;
+  scanf("%c",& ch);
+  if(ch>='A' && ch<='Z')
+  {
+    printf("the character is uppercase");
+  }
+  else if(ch>='a' && ch<='z')
+  {
+    printf("the charecter is lowercase");
+  }
   else
-{
-printf("consonant");
-  }  
+  {
+    printf("the character is not an alphabet");
+
+  }
+
+   
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
