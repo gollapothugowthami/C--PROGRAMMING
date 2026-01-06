274198f224573890e3b886e6e0c7242128ceb879
gollapothugowthami <gollapothugowthami@gmail.com>
2026-01-06 15:30:56 +0530

    Add files via upload
    c program count digits

---
+++ count digits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+long n;    
+ scanf("%d",&n);
+ int sum = 0;
+ while(n!=0){
+ int rem = n % 10;
+ sum = sum + rem;
+ n/=10 ; 
+ 
+  printf("%d", rem); 
+} 
+    return 0;
+}
