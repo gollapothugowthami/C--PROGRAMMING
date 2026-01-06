e45a979ee4c4f36014b18d3b9c2a2bcfcee0bb0a
gollapothugowthami <gollapothugowthami@gmail.com>
2026-01-06 15:04:01 +0530

    Add files via upload
    write a c program negative and positive

--- 2.c
+++ 2.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 int main() {
 int a;
-scanf("%d",&a);
-if(a>0)
-{
- printf("positive");   
+scanf("%d",& a);
+
+if(a>0){
+printf("positive");
 }
-if(a<0)
-{
- printf("negative");   
+else if(a<0){
+    printf("negative");
 }
-else
-{
-printf("zero");
-}    
+else 
+    {
+        printf("zero");
+    }
     return 0;
 }
