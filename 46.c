52d36d07a6b4a28f36b87da6dc67faff45d9d69e
gollapothugowthami <gollapothugowthami@gmail.com>
2026-01-13 08:26:52 +0530

    Add files via upload
    c program length of a string

---
+++ 46.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int length =o;
+    printf("enter a string: ");
+    fgets(str, sizeof(str),stdin);
+     while(str[length] !='\o' ) {
+ fgets   
+    if (str[length] =='\n'){
+        break;
+    }
+    length++;
+ }
+    printf("length of the string = %d\n", length);
+    
+    
+    return 0;
+}
