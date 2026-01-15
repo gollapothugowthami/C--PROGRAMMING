5a0b1b56905ea4812edc362a0a2eb17e5fe97373
gollapothugowthami <gollapothugowthami@gmail.com>
2026-01-15 18:34:58 +0530

    Add files via upload
    c program compare two strings

---
+++ 48.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char str1[100], str2[100];
+  printf("enter first string:");
+fgets(str1, sizeof(str1),stdin);
+   printf("enter second  string:");
+   fgets(str2, sizeof(str2), stdin);
+   if(strcmp(str1, str2)== 0)
+   printf("strings are equal");
+   else
+   printf("strings are not equal"); 
+    
+    return 0;
+}
