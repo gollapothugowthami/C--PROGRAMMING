d6b9ebbf2aa08d030f2c23292908435000461a67
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-07 11:42:13 +0530

    assign value.c

---
+++ assign value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int a = 10;
+float b = 10;
+char ch = 10;
+printf("integer value:%d \n", a);
+printf("float value: %.2f \n", b);
+printf("char value: %ch \n", c); 
+    
+    return 0;
+}
