51a0212aef4461b96a92fa436ad6b9d0148405af
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-09 10:44:34 +0530

    marks .c

---
+++ marks .c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+int marks;
+printf("enter marks(0 -100):");
+scanf("%d",& marks);
+if(marks<0||marks>100)
+{
+ printf("invalid marks\n");   
+}
+else if(marks >=90)
+{
+ printf("Grade:A\n");   
+}
+else if(marks >=80)
+{
+ printf("Grade:B\n");   
+}
+else if(marks >=70)
+{
+ printf("Grade:C\n");   
+}
+else if(marks >=60){
+ printf("Grade:D\n");
+}
+ return 0;
+}
