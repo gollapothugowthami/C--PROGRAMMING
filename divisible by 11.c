6b4a4c8e522ceb6c4bcc9693cdce5450d02c3cec
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-09 11:47:52 +0530

    divisible by 11.c

---
+++ divisible by 11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+int a;
+scanf("%d",& a);
+printf("%d", a); 
+if(num % 11 == 0)
+{
+    printf("yes");
+
+}  
+else if 
+{
+    printf("no");
+}
+    
+    return 0;
+}
