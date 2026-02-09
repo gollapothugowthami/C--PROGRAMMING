7475608816e4e60592a66d37c84c47459bc6d027
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-09 10:15:51 +0530

    two numbers.c

---
+++ two numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+ int a, b;
+scanf("%d %d", &a, &b);
+if(a > b)
+{
+   printf("a is greater");
+}
+else if
+{
+    printf("b is greater");
+}
+    //Your Code goes here!
+    
+    return 0;
+}
