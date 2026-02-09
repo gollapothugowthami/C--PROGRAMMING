4ce2d05a9c5738ecd37bf5f8fe56c78ace9c7575
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-09 10:08:12 +0530

    divisible by 5.c

---
+++ divisible by 5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+int a;
+scanf("%d", & a);
+printf("%d", a);
+if(num %5 == 0)
+{
+   printf("divisible by 5");
+
+}
+else if
+{
+    printf("not divisible by 5");
+}
+    
+    return 0;
+}
