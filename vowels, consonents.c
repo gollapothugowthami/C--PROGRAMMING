0322004e78e2f061f35aa68cc02d1e069484752f
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-09 10:30:17 +0530

    vowels, consonents.c

---
+++ vowels, consonents.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char ch;
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||)
+    {
+    printf("the character is a vowel");
+ }    
+else{
+    printf("the character is not a vowel");
+}
+    
+    return 0;
+}
