2d5b70daf49ad2d8f61ab24105082f69345a90e4
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-09 13:39:47 +0530

    valid triangle.c

---
+++ valid triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+ int a, b, c;
+ scanf("%d %d %d", &a, &b, &c);
+ sum = a+b+c= 180
+ if(a+b+c)
+ {
+    printf("valid triangle");
+ }
+ else if
+ {
+    printf("not an valid tiangle");
+ }
+ return 0;
+}
