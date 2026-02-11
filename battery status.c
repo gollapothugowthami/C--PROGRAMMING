be1d4197084f9c73861b2d8d3e80c12dbc4b8d33
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-11 12:05:27 +0530

    battery status.c

---
+++ battery status.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    if(n<=20)
+    {
+        printf("low battery");
+    }
+    else 
+    {
+        printf("battery ok");
+    }
+  
+
+    
+    return 0;
+}
