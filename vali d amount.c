60b0d77b3e0798825bd650eed41af140ea4f65f3
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-11 11:14:13 +0530

    vali d amount.c

---
+++ vali d amount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+int n;
+scanf("%d", & n);
+if(n %100 ==0)
+{
+    printf("valid amount");
+}
+else 
+{
+    printf("invalid amount");
+}
+    
+
+    
+    return 0;
+}
