29de349ec4892a12bf17d94a9cd867fed76dafe5
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-11 11:21:27 +0530

    weeked checker.c

---
+++ weeked checker.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int n;
+scanf("%d", & n);
+if(n >= 1 && n <= 5)
+{
+    printf("week day");
+}
+else (n >=6 && n<=7)
+{
+  printf("weekend");
+    
+    return 0;
+}
