aba23c04723c361629333e75f318cdfd3754c429
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-11 14:22:58 +0530

    eligible to vote.c

---
+++ eligible to vote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+int age;
+scanf("%d", & age);
+if(age >= 18)
+{
+    printf("eligible");
+}    
+else
+{
+    printf("not eligible");
+}
+    //Your Code goes here!
+    
+    return 0;
+}
