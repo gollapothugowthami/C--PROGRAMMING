2af24cec96ab89d11fc80e4bff6ef379cb51d2e2
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-11 12:09:17 +0530

    delivery charges.c

---
+++ delivery charges.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+int n;
+scanf("%d", & n);
+if(n >= 499)
+{
+    printf("free delivery");
+} 
+else
+{
+    printf("delivery charges apply");
+}   
+    
+    
+    return 0;
+}
