a7ce7e0e3ec68bc28f8e1a3d88731f80325ee349
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-12 14:50:06 +0530

    mobile recharge.c

---
+++ mobile recharge.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+int plan type, payment mode;
+scanf("%d %d",& plantype, &payment mode );
+switch (plan type){
+ case 1:
+ amount  = 199;
+ break;
+ case 2:
+ amount = 399;
+ break;
+
+}
+switch (payment mode){
+case 11:
+case 12:
+cashback = 20;
+break;
+case 13:
+cashback = no;
+break;    
+}
+ return 0;
+}
