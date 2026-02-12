8a17c19d205369280c02951f9d0733c74c4e28b6
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-12 13:53:38 +0530

    speed type.c

---
+++ speed type.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+int order amount, speed type;
+scanf("%d %d", &order amount, & speed type);
+switch (speed type){
+ case 1:
+ if(normal = 50)
+ {
+    printf("normal");
+ }
+ break;
+ case 2:
+ if(order <= 100)
+ {
+    printf("100");
+ }
+ break;
+ case 3:
+ else(order >= 1000)
+ {
+    printf("free");
+ }
+ break;
+ 
+      return 0;
+}
