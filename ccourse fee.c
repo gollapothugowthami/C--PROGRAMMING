a63e477d57e55c28e328a3c43715a35aac2e7212
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-12 16:24:15 +0530

    course fee.c

---
+++ ccourse fee.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+int online, offline;
+scanf("%d %d", &online, &offline);
+switch (online){
+    case 1:
+    regular = 5000;
+    break;
+    case 2:
+    scholarship =3000;
+    break;
+}
+switch (offline){
+    case 1:
+    regular =9000;
+    break;
+    case 2:
+    scholarship = 7000;
+    break;
+}   
+ return 0;
+}
