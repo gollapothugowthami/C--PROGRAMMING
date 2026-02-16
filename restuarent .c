37d4ecffe01dfaa4449779c99f58c535e8d9c01e
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-16 18:17:29 +0530

    restuarent .c

---
+++ restuarent .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+int customertype, bill amount;
+scanf("%d %d", &customertype, &bill amount);
+switch (customertype ){
+    case 1:
+    regular = 5;
+    break;
+    case 2
+    premium = 15;
+    break;
+   defalut
+   prinrtf("invalid customer type") ;
+
+}    
+
+    
+    return 0;
+}
