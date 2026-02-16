1e39a73048ac84b8632b55fc79443338cc5cb6b2
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-16 18:08:52 +0530

    movie ticket.c

---
+++ movie ticket.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+int seat type, show time;
+scanf("%d %d", &seat type, &show time);
+switch(seat type){
+   case 1:
+   silver = 150;
+   break;
+   case 2:
+   gold = 250;
+   break;
+   case 3:
+   night show(show time >= 18);
+   break;
+   defalut
+   printf("invalid seat type");
+}
+    
+    return 0;
+}
