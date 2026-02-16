0666b7340c9ff581439aa8eb62a96e2e1ee245d2
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-16 19:03:03 +0530

    online shopping.c

---
+++ online shopping.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+int delivery days;
+scanf("%d", &delivery days);
+switch (delivery days){
+    case 2:
+    standard = 40;
+    break;
+    case 2:
+    same day = 120;
+    break;
+    case 3:
+    pick up = free;
+    break;
+    default
+    printf("invalid delivery days");
+
+}  
+ return 0;
+}
+printf("delivery₹ %d", delivery days);
+return 0;
+}
