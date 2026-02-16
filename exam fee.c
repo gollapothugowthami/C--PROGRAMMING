ad1385e58a918c0b06d3af43106e142cbd5efecf
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-16 18:30:45 +0530

    exam fee.c

---
+++ exam fee.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+int category;
+scanf("%d", &category);
+switch (student category){
+    case 1:
+   if general = 1200;
+    break;
+    case 2:
+    OBC = 800;
+    break;
+    case 3:
+    SC/ST = 500;
+    break;
+    defalut 
+    printf("invalid category");
+}    
+    
+    
+    return 0;
+}
