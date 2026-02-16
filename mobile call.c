6ad9e0f3b38a5c2af813fdd612fb095f0dfcf5b7
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-16 18:23:45 +0530

    mobile call.c

---
+++ mobile call.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+ int call type, minutes;
+ scanf("%d %d", &call type, &minutes);
+ switch (call type){
+    case 1:
+    local = 1;
+    break;
+    case 2:
+    std = 3;
+    break;
+    case 3:
+    isd = 10;
+    default 
+    printf("invalid call type");
+ } 
+    
+    return 0;
+} 
