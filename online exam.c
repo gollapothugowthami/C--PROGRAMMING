2a663d7f72201945d3c560eacbe6dd57f2496893
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-16 17:44:21 +0530

    online exam.c

---
+++ online exam.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+int marks;
+scanf("%d", & marks);
+switch (marks){
+case 1:    
+ if(marks >= 80){
+ printf("excellent");   
+}
+break;
+case 2:
+if(marks >= 60){
+printf("good");
+}
+break;
+case 3:
+default (needs improvement)
+ return 0;
+}
