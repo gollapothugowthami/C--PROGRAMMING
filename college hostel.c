2cf3abef69b37f02eecbdb243983034515521820
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-16 19:15:58 +0530

    college hostel.c

---
+++ college hostel.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    int  sharing type;
+    scanf("%d", &hostel fee);
+    switch (hostel fee){
+     case 1:
+     if(room = ₹80000){
+     printf("single");}
+
+     break;
+     case 2:
+     else if(room = ₹60000){
+    printf("double");}
+     break;
+     case 3:
+     else(room =₹45000){
+printf("triple");
+     }
+
+    
+     break;
+     defalut:
+     printf("invalid hostel fee");
+       
+    }
+     return 0;
+}
+printf("hostel ₹%d", hostel fee);
+return 0;
+}
