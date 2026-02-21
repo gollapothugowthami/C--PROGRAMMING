48f6f6c03bd7c63d880d3076a745a83de6bee5f6
gollapothugowthami <gollapothugowthami@gmail.com>
2026-02-21 10:12:19 +0530

    fibonacci series.c

---
+++ fibonacci series.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int a =0; b = 1; temp;
+printf("fibanocci series");
+for(int i = 0; i <n; i++){
+    temp = a+b;
+    a = b;
+    b = temp;
+    printf("the %d digits are",n,temp);
+
+} 
+    
+    return 0;
+}
