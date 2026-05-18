9c5369feb3eee26fb1911936aff7decd127d3c6c
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-18 14:06:43 +0530

    v.c
    v.c

---
+++ v.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+printf("My name is gowthami\v");
+printf("today is May 18th 2026 monday\v");
+printf("Sangagiri is very hot today\v");
+printf("some students are late\v");
+printf("late habit is very bad\v");
+printf("late coming will not accepted\v");
+printf("two girls are not doing work\v");
+   
+    //Your Code goes here!
+    
+    return 0;
+}
