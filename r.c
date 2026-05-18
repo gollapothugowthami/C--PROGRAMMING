379a6e2eda687f06400fdb8a77318525c73a6efe
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-18 14:17:05 +0530

    Add files via upload
    r.c

---
+++ r.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+ printf("Hello How are you\r");
+ printf("My name is gowthami\r");
+ printf(" today  Iam attending c promming class\r");
+ printf("NOW today to 18 days progrram\r");   
+    //Your Code goes here!
+    
+    return 0;
+}
