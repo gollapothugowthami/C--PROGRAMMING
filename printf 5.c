23bb6fb0da27600af111a4d18476b4776714f83b
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-19 10:09:09 +0530

    Add files via upload
    printf 5

---
+++ printf 5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+printf("Hello How are you?\v"); 
+printf("My name is gowthami\v");
+printf("Today is 19th May 2026\v");
+printf("Iam at SSCET SALAM \v");
+printf("students are learning coding\v");
+printf("for some time there was no power\v");
+printf("now power is okay\v");
+printf("some students are coming late\v");
+printf("sir was shoulting them\v");
+printf("sir told the one world\v");
+printf("time sence is very important\v");
+printf("Today we dont have class\v");
+printf("But yesterday we have class\v");
+printf("In yesterday we learn\v");
+printf("about c promming\v");
+printf("Data types are learn\v");
+printf("sir teaching is very good\v");
+printf("I learn new things in that class\v");
+printf("IN that class the two girls are not listening\v");
+printf("sir was watching\v");
+printf("shoulting the sir with two girls\v");
+printf("girls are went the class\v");
+printf("after that going to the hostel\v");
+
+
+    
+    return 0;
+}
