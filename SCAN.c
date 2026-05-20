ae5618c397341a8ab368f37560fdd5da11165826
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-20 16:03:17 +0530

    Add files via upload
    scan

---
+++ SCAN.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+ int A, B, Answer;
+printf("ENTER THE VALUE FOR A:\n");
+scanf("%d",& A);
+  printf("..........................\n");  
+printf("Hello My friend you enterted %d:\n", A);
+ printf("ENTER THE VALUE FOR B:\n");
+scanf("%d",& B); 
+printf("..........................\n");  
+printf("Hello My friend you enterted %d:\n", B);   
+    //Your Code goes here!
+    
+    return 0;
+}
