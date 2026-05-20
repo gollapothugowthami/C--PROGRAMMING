87d89da2633c30e46d69c14e683763c46f2b53f2
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-20 11:30:16 +0530

    Add files via upload
    charecter

---
+++ charecter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+ /*charecter variable decleration*/
+ int A= 20;
+ int B =50;
+ int c= A+B;
+ printf("The value of c is = %i", c);   
+    //Your Code goes here!
+    
+    return 0;
+}
