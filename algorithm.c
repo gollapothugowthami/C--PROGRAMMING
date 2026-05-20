38873971f11913aba315fcc9d61c8d64f9f22272
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-20 15:01:10 +0530

    Add files via upload
    algorithm

---
+++ algorithm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int main() {
+int A=2000;
+int B =4000;
+int C=A+B;
+printf("Result of addition %d:/n/n", A+B);
+ printf("Result of subtraction %d:/n/n", A-B);
+printf("Result of multiplication %d:/n/n", A*B);
+printf("Result of division %d:/n/n", A%B);
+
+    //Your Code goes here!
+    
+    return 0;
+}
