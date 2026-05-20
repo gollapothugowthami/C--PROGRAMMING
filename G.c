7031bde0255c190090cc1872e3246f7720ebec5b
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-20 16:02:50 +0530

    Add files via upload
    g.c

---
+++ G.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int A, B, Answer;
+printf("ENTER THE VALUE FOR A:\n");
+scanf("%d",& A);
+  printf("..........................\n");  
+printf("Hello My friend you enterted %d:\n", A);
+  
+    return 0;
+}
