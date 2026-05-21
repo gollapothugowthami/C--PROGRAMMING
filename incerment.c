d3ec6718d75b4df3f482db5305f9008a7fd3e40c
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-21 11:20:23 +0530

    Add files via upload
    increment

---
+++ incerment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*incrementing & decreament methods */
+int main() {
+ int a, b, c;
+ a = 10;
+ b = a +1;
+ 
+ printf("The value of A, B  %d %d ",a,b);   
+
+
+    
+    return 0;
+}
