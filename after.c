dc711a3b3cb55daec91204e0e985f595bd4ba68d
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-21 11:23:01 +0530

    Add files via upload
    after

---
+++ after.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+/*INCREMENT OPERATOR */
+int a = 10;
+printf("The value of A Now is %d:\n", a);
+printf("The value of After increment is %d:\n", a);
+
+    //Your Code goes here!
+    
+    return 0;
+}
