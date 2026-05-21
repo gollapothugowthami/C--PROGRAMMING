66862870e108c414e682cc6870c3a5895d7e234d
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-21 11:23:25 +0530

    Add files via upload
    100

---
+++ 100.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+/* INCREMENTAL OPERATION DIFFERENCES */
+int a = 100;
+printf("The value of A Now is %d:\n", a);   
+++a;
+printf("The value of AFTER INCREMENT is %d: \n", a); 
+    //Your Code goes here!
+    
+    return 0;
+}
