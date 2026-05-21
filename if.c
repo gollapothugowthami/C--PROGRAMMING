37bcdabb8de06096c19b827bead72ae41d796ee1
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-21 12:48:04 +0530

    Add files via upload
    if

---
+++ if.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int A;
+printf("ENTER YOUR MARKS");
+scanf("%d",&A);    
+if(A > 50)
+{
+ printf("PASS");   
+}    
+else{
+    printf("FAIL");
+}
+    return 0;
+}
