0b93c5a2b76f5a92ad96c7497606947d5ce1fe92
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-21 11:23:56 +0530

    Add files via upload
    conditional

---
+++ conditional.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ /* SIMPLE CONDITIONS IF-ELSE*/
+ int main(){
+ int A= 100;
+ int B =200;
+ if(A > B){
+ printf("Ais greater than B");
+   
+ }
+ else{
+ printf("Ais smaller than B");
+ }
+ return 0;
+}
