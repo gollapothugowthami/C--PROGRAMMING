1ae617edbca99c3f8e8a35ee45ee9e346f699045
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-21 11:49:28 +0530

    Add files via upload
    else

---
+++ else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int A=100;
+int B=200;
+if(A>B){
+    printf("A=%d and B =%d hence Ais Greater than B", A,B);
+}
+else{
+    printf("A=%d and B =%d hence Ais Greater than B", A,B); 
+}
+
+    //Your Code goes here!
+    
+    return 0;
+}
