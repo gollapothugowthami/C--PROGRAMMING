532bd6b35e8d2b3d46424b7b4b08c5adea108b25
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-21 11:22:39 +0530

    Add files via upload
    before increment

---
+++ before increment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+int a, b, c;
+a = 10;
+printf("The value of a BEFORE increment is %d", a);    
+ /* increment A */
+ printf("The value of a AFTER increment is %d", a); 
+ S = a-1;
+ printf("The value of a AFTER increment is %d", a);  
+    
+    return 0;
+}
