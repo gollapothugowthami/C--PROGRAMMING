9c2f2ee218e1a91f05346fcc6b29d2e7b59e8ade
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-22 11:39:33 +0530

    Add files via upload
    area

---
+++ area 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int r;
+float pi= 3.1459;
+float area;
+printf("Input radius:");
+scanf("%d",& r);
+area = pi*r*r;
+printf("The area for given r = %d is =%f", r, area);
+    //Your Code goes here!
+    
+    return 0;
+}
