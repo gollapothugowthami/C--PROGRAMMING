d703aa66e62fd851b3c6d76ae97653d4ce0e0245
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-22 11:52:16 +0530

    Add files via upload
    float

---
+++ 2 float.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <float.h> 
+int main() {
+printf("THE MINI NUM VALUE OF FLOAT IS = %f", FLT_MIN);
+printf("THE MAX NUM VALUE OF FLOAT IS = %f", FLT_MAX);
+    //Your Code goes here!
+    
+    return 0;
+}
