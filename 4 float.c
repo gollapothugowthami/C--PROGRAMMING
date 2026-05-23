ebeb2dca7e05d5878dd825e6d03ad18a65773d75
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-23 10:55:16 +0530

    Add files via upload
    4 float

---
+++ 4 float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+float Multiplication(float i1, float i2, float i3)
+{
+ return(i1 *i2 *i3);
+}
+int main() {
+ printf("Multiplication is %f\n", multiplication(4, 6, 12));  
+} 
+    return 0;
+}
