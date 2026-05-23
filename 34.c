352bbfd91bedb691f8dd60e77b776cb28342c96d
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-23 14:24:23 +0530

    Add files via upload
    34

--- 34.c
+++ 34.c
@@ -1,16 +1,25 @@
-#include<stdio.h>
-int main()
-{
-    int n,i,a[50],min;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    min = a[0];
-    for(i=0;i<n;i++)
-    {
-        if(a[i]<min)
-        min = a[i];
+#include <stdio.h>
+struct class {
+    int number;
+    char name[20];
+    float marks;
+};
+
+int main() {
+    int x;
+    struct class student1 = {111, "Rao", 98.31};
+    struct class student2 = {222, "Reddy", 94.36};
+    struct class student3;
+
+    student3 = student2;
+
+    x = ((student3.number == student2.number) && 
+        (student3.marks == student2.marks)) ? 1 : 0;
+
+    if (x == 1) {
+        printf("\nstudent2 and student3 are same\n\n");
+        printf("%d %s %f\n", student3.number, student3.name, student3.marks);
+    } else {
+        printf("\nstudent2 and student3 are different\n\n");
     }
-    printf("%d",min);
-    return 0;
 }
