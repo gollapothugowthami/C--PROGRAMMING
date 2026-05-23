ff26c978e341b21ae9624cf2fbc412c38b1c176f
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-23 14:24:02 +0530

    Add files via upload
    student

---
+++ student.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int number;
+char name[20];
+float marks;
+
+int main()
+{
+    int x;
+    struct class student1=(111,"RAO",72.50);
+struct class student2=(222,"REDDY",67.00);
+struct class student3;
+student 3 - student 2;
+x=((student 3.number==student 2.number))&&
+((student 3 .marks==student 2. marks))?1:0;
+if(x==1)
+{
+    printf("\n student 2 and student 3 are same\n\n");
+    printf("%d %s %f \n", student 3. number,student 3. name, student 3 .marks);
+}
+else
+printf("\nstudent 2 and student 3")
+}
+  
+    return 0;
+}
