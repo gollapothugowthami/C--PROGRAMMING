4f5717bc2cbc2f1fe81fb2e594a1a3642de1f3ec
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-23 14:42:30 +0530

    Add files via upload
    stop

---
+++ stop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int number, sq;
+printf("enter the number:");
+scanf("%d",&number);
+sq = number *number;
+printf("\nsquare = %d", sq);    
+    //Your Code goes here!
+    
+    return 0;
+}
