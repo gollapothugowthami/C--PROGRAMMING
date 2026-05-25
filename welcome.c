5f0841ed9b91cf0b1c101707558ad52fda386601
gollapothugowthami <gollapothugowthami@gmail.com>
2026-05-25 16:41:02 +0530

    welcome.c

---
+++ welcome.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name1[20] = "GOWTHAMI";
+
+    printf("The length of my name is = %d characters", strlen(name1));
+char name2[20] = "VYSHNAVI";
+
+    printf("The length of my name is = %d characters", strlen(name2));
+char name3[20] = "RANI";
+
+    printf("The length of my name is = %d characters", strlen(name3));
+char name4[20] = "PALACHANDRAO";
+
+    printf("The length of my name is = %d characters", strlen(name4));
+char name5[20] = "SITA";
+
+    printf("The length of my name is = %d characters", strlen(name5));
+char name6[20] = "ANU";
+
+    printf("The length of my name is = %d characters", strlen(name6));
+char name7[20] = "AMMULU";
+
+    printf("The length of my name is = %d characters", strlen(name7));
+     char name8[20] = "AMULYA";
+
+    printf("The length of my name is = %d characters", strlen(name8));
+char name9[20] = "VIKY";
+
+    printf("The length of my name is = %d characters", strlen(name9));
+char name10[20] = "VINAY";
+
+    printf("The length of my name is = %d characters", strlen(name10));
+char name11[20] = "SRAVANI";
+
+    printf("The length of my name is = %d characters", strlen(name11));
+char name12[20] ="LAKSHMI";
+
+    printf("The length of my name is = %d characters", strlen(name12));
+char name13[20] = "SUBBU";
+
+    printf("The length of my name is = %d characters", strlen(name13));
+char name14[20] = "PINKY";
+
+    printf("The length of my name is = %d characters", strlen(name14));
+    char name15[20] = "SILPA";
+
+    printf("The length of my name is = %d characters", strlen(name15));
+char name16[20] = "SHETTY";
+
+    printf("The length of my name is = %d characters", strlen(name16));
+char name17[20] = "SAI";
+
+    printf("The length of my name is = %d characters", strlen(name17));
+char name18[20] = "RAMYA";
+
+    printf("The length of my name is = %d characters", strlen(name18));
+char name19[20] = "JOSHNNA";
+
+    printf("The length of my name is = %d characters", strlen(name19));
+char name20[20] = "ARUN";
+
+    printf("The length of my name is = %d characters", strlen(name20));
+
+    return 0;
+}
