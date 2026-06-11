42ac61196a5009e702c2a553bb6c5f26eff955e9
gollapothugowthami <gollapothugowthami@gmail.com>
2026-06-11 23:26:43 +0530

    Add files via upload

---
+++ except.py
@@ -0,0 +1,7 @@
+try:
+      a = int(input("enter  first number":))
+      b = int(input("enter second number":)) 
+      result= a / b
+      print("division=", result)
+ except(zero division error,value error):
+      print("error occured")    
