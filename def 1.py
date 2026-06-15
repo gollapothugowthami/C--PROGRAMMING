af7ea2a167cbe9278c6df16cc1907d5953df2728
gollapothugowthami <gollapothugowthami@gmail.com>
2026-06-15 21:46:56 +0530

    Add files via upload
    def

---
+++ def 1.py
@@ -0,0 +1,18 @@
+def sum1():
+  a = 10
+  b = 20
+  print("sum =", a+b)
+def sum2():
+  print("sum =",a+b)
+def sum3():
+  a = 30
+  b = 40
+  return a+b
+def sum4():
+  return a+b
+sum1()
+sum2()
+result1 = sum3()
+print("sum =",result1)
+result2 = sum4()
+print("sum =",result2)                
